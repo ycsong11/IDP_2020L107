7d8d7b474bb192571c7901ac375a00bf45d56068
mjc284 <mjc284@cam.ac.uk>
2020-01-22 12:28:07 +0000

    filled in sub-classes, wrote state0

--- main/main.ino
+++ main/main.ino
@@ -1,61 +1,136 @@
 #include <Servo.h>
+Servo servo1;
+Servo servo2;
 
-int servo_motor1 =
-int servo_motor2 =
-int power_motorL =
-int power_motorR =
-int arm_motor =
-int distance_sensor1 =
-int distance_sensor2 =
-int light_sensor =
+int servo_motor1 = 1;
+int servo_motor2 = 2;
+int power_motorL = 3;
+int power_motorR = 4;
+int arm_motor = 5;
+int distance_sensor1 = A1;
+int distance_sensor2 = A2;
+int light_sensorL = A3;
+int light_sensorR = A4;
 
 class chassis {
   public:
-  int right = power_motorR;
-  int left = power_motorL;
   int traverse_speed = 50; //pwm of 0-255
   int rotate_speed = 10; //pwm of 0-255
-  void traverse(bool front) //if front = 1, forward
+  int rotate90_duration = 1000; //TODO: measure how long to rotate 90 deg
+  void traverse(bool front) //if front = 1, go forward
   {
     if(front == 1)
     {
-      analogWrite(right, traverse_speed);
-      analogWrite(left, traverse_speed);
+      analogWrite(power_motorR, traverse_speed);
+      analogWrite(power_motorL, traverse_speed);
     }
     else //figure out how to drive motor backwards
     {
-      analogWrite(right, -1 * traverse_speed);
-      analogWrite(left, -1 * traverse_speed);
+      analogWrite(power_motorR, -1 * rotate_speed);
+      analogWrite(power_motorL, -1 * rotate_speed);
     }
   }
-  void rotate(bool right) //if right = 1, rightward
+  void rotate(bool right) //if right = 1, turn rightward
   {
     if(right == 1)
     {
-      analogWrite(right, -1 * traverse_speed);
-      analogWrite(left, traverse_speed);
+      analogWrite(power_motorR, -1 * traverse_speed);
+      analogWrite(power_motorL, traverse_speed);
     }
     else //figure out how to drive motor backwards
     {
-      analogWrite(right, traverse_speed);
-      analogWrite(left, -1 * traverse_speed);
+      analogWrite(power_motorR, traverse_speed);
+      analogWrite(power_motorL, -1 * traverse_speed);
     }
   }
-}
+  void rotate90(bool right)
+  {
+    if(right == 1)
+    {
+      analogWrite(power_motorR, -1 * traverse_speed);
+      analogWrite(power_motorL, traverse_speed);
+    }
+    else //figure out how to drive motor backwards
+    {
+      analogWrite(power_motorR, traverse_speed);
+      analogWrite(power_motorL, -1 * traverse_speed);
+    }
+    delay(rotate90_duration);
+    digitalWrite(power_motorR, LOW);
+    digitalWrite(power_motorL, LOW);
+  }
+  void halt()
+  {
+    digitalWrite(power_motorR, LOW);
+    digitalWrite(power_motorL, LOW);
+  }
+};
 
 class rescue_arm {
   public:
-
-}
+  int motor_power = 50; //pwm of 0-255 TODO:callibrate this
+  void hold()
+  {
+    analogWrite(arm_motor, motor_power);
+  }
+  void relax()
+  {
+    digitalWrite(arm_motor, 0);
+  }
+};
 
 class sensors {
   public:
-  
-}
+  int color_threshold = 490; //TODO: callibrate this
+  int detect_white() //0 if both black, 1 if left white, 2 if right white, 3 if both white
+  {
+    if((analogRead(light_sensorL) < color_threshold) and  (analogRead(light_sensorR) < color_threshold)){return 0;}
+    else if((analogRead(light_sensorL) > color_threshold) and  (analogRead(light_sensorR) < color_threshold)){return 1;}
+    else if((analogRead(light_sensorL) < color_threshold) and  (analogRead(light_sensorR) > color_threshold)){return 2;}
+    else if((analogRead(light_sensorL) > color_threshold) and  (analogRead(light_sensorR) > color_threshold)){return 3;}
+  }
+  float detect_distance1() //return distance from sensor 1 in cm 
+  {
+    float sensorValue = analogRead(distance_sensor1);
+    float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
+    cm = roundf(cm);
+    return cm;
+  }
+  float detect_distance2() //return distance from sensor 2 in cm 
+  {
+    float sensorValue = analogRead(distance_sensor2);
+    float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
+    cm = roundf(cm);
+    return cm;
+  }
+  void radar_angle(int angle) //TODO: test this
+  {
+    servo1.write(angle);
+  }
+};
 
 void state0(int* state)
 {
+  //initialize
+  servo1.write(90);
+  servo2.write(90);
+  digitalWrite(power_motorL, LOW);
+  digitalWrite(power_motorR, LOW);
+  digitalWrite(arm_motor, LOW);
   
+  while(sensors.detect_white() != 2)
+  {
+    if(sensors.detect_white() != 0){//throw color sensor calliration error}
+    chassis.traverse(1);
+  }
+  chassis.rotate90(1);
+  while(sensors.detect_white() != 3)
+  {
+    if(sensors.detect_white() != 0){//throw color sensor calliration error}
+    chassis.traverse(1);
+  }
+  chassis.halt();
+  *state = 1;
 }
 
 void state1(int* state)
@@ -78,7 +153,7 @@ void state4(int* state)
   
 }
 
-void state4(int* state)
+void state5(int* state)
 {
   
 }
@@ -92,17 +167,16 @@ void setup() {
   pinMode(arm_motor, OUTPUT);
   pinMode(distance_sensor1, INPUT); 
   pinMode(distance_sensor2, INPUT); 
-  pinMode(light_sensor, INPUT); 
+  pinMode(light_sensorL, INPUT); 
+  pinMode(light_sensorR, INPUT);
 }
 
 int state = 0;
+chassis chassis;
+rescue_arm rescue_arm;
+sensors sensors;
+
 void loop() {
-//initialize
-servo1.write(0);
-servo2.write(0);
-digitalWrite(power_motorL, LOW);
-digitalWrite(power_motorR, LOW);
-digitalWrite(arm_motor, LOW);
 
   switch(state){
     case 0:
@@ -124,4 +198,5 @@ digitalWrite(arm_motor, LOW);
       state5(&state);
       break;
   }
+
 }
