4f2ac0341f9a1e0e461adc0be14af351a6102f7b
mjc284 <mjc284@cam.ac.uk>
2020-01-24 19:22:48 +0000

    created more tests

--- test/Victim_sensing/victim_sensing.ino
+++ test/Victim_sensing/victim_sensing.ino
@@ -119,142 +119,9 @@ chassis chassis;
 rescue_arm rescue_arm;
 sensors sensors;
 
-void state0(int* state)
-{
-  //initialize
-  servo1.write(90);
-  servo2.write(90);
-  chassis.halt();
-  digitalWrite(arm_motor, LOW);
-  delay(100);
-
-  
-  while(sensors.detect_white() != 2)
-  {
-    //if(sensors.detect_white() != 0){//throw color sensor calliration error}
-    chassis.traverse(1);
-  }
-  delay(10);
-  chassis.rotate90(1);
-  delay(10);
-  while(sensors.detect_white() != 3)
-  {
-    //if(sensors.detect_white() != 0){//throw color sensor calliration error}
-    chassis.traverse(1);
-  }
-  chassis.halt();
-  *state = 1;
-}
-
-void state1(int* state)
-{
-  int location = 3; //0 = at center, 1 = at left, 2 = at right, 3 = error
-  float distance_to_wall = 100; //TODO: callibrate this
-  int angle = 80;
-  sensors.radar_angle(angle);
-  while(angle < 100)
-  {
-    float distance1 = sensors.detect_distance1();
-    float distance2 = sensors.detect_distance2();
-    if ((distance1 < distance_to_wall) and (distance2 < distance_to_wall))
-    {
-      location = 0;
-    }
-    sensors.radar_angle(angle);
-    angle++;
-    delay(10);
-  }
-  if(location == 0)
-  {
-    sensors.radar_angle(90);
-    while((sensors.detect_distance1() > 5) and (sensors.detect_distance2() > 5))
-    {
-      chassis.traverse(1);
-    }
-    chassis.halt();
-    *state = 2;
-  }
-  else
-  {
-    bool at_left = 0;
-    float cnt = 0;//TODO: callibrate this
-    float distance_to_side = 100; //TODO: callibrate this
-    sensors.radar_angle(0);
-    while(cnt < 10000)
-    {
-      chassis.traverse(1);
-      if((sensors.detect_distance1() < distance_to_side) and (sensors.detect_distance2() < distance_to_side))
-      {
-        at_left = 1;
-        break;
-      }
-    }
-    chassis.halt();
-    if(at_left == 1)
-    {
-      chassis.rotate90(0);
-      while((sensors.detect_distance1() > 5) and (sensors.detect_distance2() > 5))
-      {
-        chassis.traverse(1);
-      }
-      chassis.halt();
-      *state = 2;
-    }
-    else
-    {
-      chassis.rotate90(0);
-      chassis.rotate90(0);
-      int at_right;
-      cnt = 0;
-      while(cnt < 10000)
-      {
-        chassis.traverse(1);
-        if((sensors.detect_distance1() < distance_to_side) and (sensors.detect_distance2() < distance_to_side))
-        {
-          at_right = 1;
-          break;
-        }
-      }
-      if(at_right == 1)
-      {
-        chassis.rotate90(0);
-        while((sensors.detect_distance1() > 5) and (sensors.detect_distance2() > 5))
-        {
-          chassis.traverse(1);
-        }
-        chassis.halt();
-        *state = 2;
-      }
-      else
-      {
-        //throw victim not found error
-      }
-    }
-  }
-}
-
-void state2(int* state)
-{
-  
-}
-
-void state3(int* state)
-{
-  
-}
-
-void state4(int* state)
-{
-  
-}
-
-void state5(int* state)
-{
-  
-}
-
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   servo1.attach(servo_motor1); 
   servo2.attach(servo_motor2); 
   pinMode(power_motorL, OUTPUT); 
@@ -270,25 +137,34 @@ int state = 0;
 
 void loop() {
 
-  switch(state){
-    case 0:
-      state0(&state); 
-      break;
-    case 1:
-      state1(&state);
-      break;
-    case 2:
-      state2(&state);
-      break;
-    case 3:
-      state3(&state);
-      break;
-    case 4:
-      state4(&state);
-      break;
-    case 5:
-      state5(&state);
-      break;
+  float threshold = 100; //TODO: callibrate this
+  int angle = 0;
+  sensors.radar_angle(angle);
+  while(angle < 180)
+  {
+    float distance1 = sensors.detect_distance1();
+    float distance2 = sensors.detect_distance2();
+    if ((distance1 < threshold) and (distance2 < threshold))
+    {
+      Serial.println("Close");
+    }
+    else {Serial.println("Far");}
+    sensors.radar_angle(angle);
+    angle++;
+    delay(10);
+  }
+  while(angle > 0)
+  {
+    float distance1 = sensors.detect_distance1();
+    float distance2 = sensors.detect_distance2();
+    if ((distance1 < threshold) and (distance2 < threshold))
+    {
+      Serial.println("Close");
+    }
+    else {Serial.println("Far");}
+    sensors.radar_angle(angle);
+    angle--;
+    delay(10);
   }
 
 }
